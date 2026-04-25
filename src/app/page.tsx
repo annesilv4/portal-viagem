0f11a4fd5ac0135c1fa1ad7975327d3488fb4bd9
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-04-25 12:34:48 -0300

    adiciona componente Home com estilos e conteúdo inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,17 @@
+import Link from 'next/link';
+import Style from './Home.module.css';
+
 export default function Home() {
+    return (
+        <main className={Style.hero}>
+            <div>
+                <h1 className={Style.hero__title}>Descubra os melhores destinos para as suas viagens</h1>
+                <p className={Style.hero__description}>Explore uma variedade de destinos incríveis e planeje suas próximas aventuras com facilidade.</p>
+
+                <Link href="/destinos" className={Style.hero__button}>
+                    Ver destinos
+                </Link>
+            </div>
+        </main>
+    )
 }
